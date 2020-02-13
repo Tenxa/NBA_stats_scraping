efe903f4e7e5a127ff1413bb5df9fbe0f49732b3
Teemu Lindgren <43570846+Tenxa@users.noreply.github.com>
2020-02-13 23:46:19 +0200

    Adds Rank, Player and Team to its own columns in the .csv file

--- nbaStats.py
+++ nbaStats.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+from itertools import zip_longest
 
 # Getting the data from ESPN
 data = requests.get('https://www.espn.com/nba/stats/player/_/table/offensive/sort/avgPoints/dir/desc')
@@ -11,16 +12,24 @@ soup = BeautifulSoup(data.text, 'html.parser')
 statTable = soup.find('table')
 tbody = statTable.find('tbody')
 
-rows = []
+rankList = []
+playerList = []
+teamList = []
+
 # Looping through all the rows and getting rank, player and team data.
 for tr in tbody.find_all('tr'):
     rank = tr.find_all('td')[0].text
+    rankList.append(rank)
     player = tr.find_all('td')[1].find_all('a')[0].text
+    playerList.append(player)
     team = tr.find_all('td')[1].find_all('span')[0].text
-    rows.append(rank+"," +player+"," +team)
+    teamList.append(team)
 
 
+# Create and write nbaStats.csv file.
+d = [rankList, playerList, teamList]
+export_data = zip_longest(*d, fillvalue= '')
 with open('nbaStats.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-    for row in rows:
-        writer.writerow([row])
+    writer.writerow(('Rank', 'Player', 'Team'))
+    writer.writerows(export_data)
