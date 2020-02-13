56a331812e0964db68d7779fd51a6cdb6dfff077
Teemu Lindgren <43570846+Tenxa@users.noreply.github.com>
2020-02-13 23:34:25 +0200

    appends every row to rows list and creates 'nbastats.csv' file with all the rows in it

--- nbaStats.py
+++ nbaStats.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 # Getting the data from ESPN
 data = requests.get('https://www.espn.com/nba/stats/player/_/table/offensive/sort/avgPoints/dir/desc')
@@ -10,9 +11,16 @@ soup = BeautifulSoup(data.text, 'html.parser')
 statTable = soup.find('table')
 tbody = statTable.find('tbody')
 
+rows = []
 # Looping through all the rows and getting rank, player and team data.
 for tr in tbody.find_all('tr'):
     rank = tr.find_all('td')[0].text
     player = tr.find_all('td')[1].find_all('a')[0].text
     team = tr.find_all('td')[1].find_all('span')[0].text
-    print(rank, player, team)
+    rows.append(rank+"," +player+"," +team)
+
+
+with open('nbaStats.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    for row in rows:
+        writer.writerow([row])
