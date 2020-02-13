d93e43f754b5b1ffe359cb23357bc41c5cb542fd
Teemu Lindgren <43570846+Tenxa@users.noreply.github.com>
2020-02-13 22:49:21 +0200

    Finding and printing all the player rankins

--- nbaStats.py
+++ nbaStats.py
@@ -3,3 +3,13 @@ from bs4 import BeautifulSoup
 
 # Getting the data from ESPN
 data = requests.get('https://www.espn.com/nba/stats/player/_/table/offensive/sort/avgPoints/dir/desc')
+
+# Load the data into BeautifulSoup
+soup = BeautifulSoup(data.text, 'html.parser')
+
+statTable = soup.find('table')
+tbody = statTable.find('tbody')
+
+for tr in tbody.find_all('tr'):
+    rank = tr.find_all('td')[0].text
+    print(rank)
